e4d09ddbb60cbae728c4d22f9e9aaf1782816fe6
Richard Huynh <emailofrichardh@gmail.com>
2025-09-15 21:28:50 -0400

    Complete register page

--- index.js
+++ index.js
@@ -1,12 +1,127 @@
+import bcrypt from "bcrypt";
+import env from "dotenv";
 import express from "express";
+import GoogleStrategy from "passport-google-oauth2";
+import passport from "passport";
+import session from "express-session";
+import { Strategy } from "passport-local";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+env.config();
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.urlencoded({ extend: true }));
 app.use(express.static("public"));  
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+db.connect();
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs"); 
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/tasks",
+  passport.authenticate("google", {
+    successRedirect: "/tasks",
+    failureRedirect: "/login",
+  })
+);
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    if (checkResult.rows.length > 0) {
+      res.redirect("/login");
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [email, hash]
+          );
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            console.log("success");
+            res.redirect("/tasks");
+          });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.redirect("/register");
+  }
+});
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/tasks",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const result = await db.query(
+          "SELECT * FROM users WHERE email = $1", [profile.email]
+        );
+        if (result.rows.length === 0) {
+          const newUser = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+            [profile.email, "google"]
+          );
+          return cb(null, newUser.rows[0]);
+        } else {
+          return cb(null, result.rows[0]);
+        }
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 app.listen(port, () => {
