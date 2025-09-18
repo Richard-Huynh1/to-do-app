12b25a23c7ab0193bd55e2e697411f89ff220b10
Richard Huynh <emailofrichardh@gmail.com>
2025-09-17 22:30:15 -0400

    Finish login page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import env from "dotenv";
 import express from "express";
+import flash from "connect-flash";
 import GoogleStrategy from "passport-google-oauth2";
 import passport from "passport";
 import session from "express-session";
@@ -25,6 +26,8 @@ app.use(express.static("public"));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(flash());
+
 const db = new pg.Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
@@ -42,20 +45,46 @@ app.get("/register", (req, res) => {
   res.render("register.ejs"); 
 });
 
+app.get("/login", (req, res) => {
+  res.render("login.ejs", { message: req.flash("error") });
+});
+
 app.get(
-  "/auth/google",
-  passport.authenticate("google", {
+  "/auth/google/register",
+  passport.authenticate("google-register", {
     scope: ["profile", "email"],
   })
 );
 
 app.get(
-  "/auth/google/tasks",
-  passport.authenticate("google", {
-    successRedirect: "/tasks",
-    failureRedirect: "/login",
+  "/auth/google/login",
+  passport.authenticate("google-login", {
+    scope: ["profile", "email"],
   })
 );
+
+app.get(
+  "/auth/google/tasks-register",
+  passport.authenticate("google-register", {
+    successRedirect: "/tasks",
+    failureRedirect: "/register",
+  })
+);
+
+app.get(
+  "/auth/google/tasks-login",
+  passport.authenticate("google-login", {
+    successRedirect: "/tasks",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/tasks",
+  failureRedirect: "/login",
+  failureFlash: true,
+}));
 
 app.post("/register", async (req, res) => {
   const email = req.body.username;
@@ -89,12 +118,40 @@ app.post("/register", async (req, res) => {
 });
 
 passport.use(
-  "google",
+  "local",
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1", [username]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            return cb(err);
+          } else {
+            if (valid) {
+              return cb(null, user);
+            } else {
+              return cb(null, false, { message: "Incorrect password" });
+            }
+          }
+        });
+      } else {
+        return cb(null, false, { message: "User not found" });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+);
+
+passport.use(
+  "google-register",
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/tasks",
+      callbackURL: "http://localhost:3000/auth/google/tasks-register",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
@@ -108,6 +165,32 @@ passport.use(
             [profile.email, "google"]
           );
           return cb(null, newUser.rows[0]);
+        } else {
+          return cb(null, result.rows[0]);
+        }
+      } catch (err) {
+        return cb(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  "google-login",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/tasks-login",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const result = await db.query(
+          "SELECT * FROM users WHERE email = $1", [profile.email]
+        );
+        if (result.rows.length === 0) {
+          return cb(null, false, { message: "Google account not registered" });
         } else {
           return cb(null, result.rows[0]);
         }
